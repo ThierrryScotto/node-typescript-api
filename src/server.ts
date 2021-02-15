02d9ccd974e27b6972de96580902360bae4bd557
thierrryScotto <thierry_192@outlook.com>
2021-02-15 02:39:47 -0300

    added mongo and mongoose

--- src/server.ts
+++ src/server.ts
@@ -3,28 +3,38 @@ import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/forecast';
 import { Application } from 'express';
+import * as database from '@src/database';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
     super();
-  }
+  };
 
-  public init(): void {
+  public async init(): Promise<void> {
     this.SetupExpress();
     this.SetupControllers();
-  }
+    await this.databaseSetup();
+  };
 
   private SetupExpress(): void {
     this.app.use(bodyParser.json());
-  }
-
-  public GetApp(): Application {
-    return this.app;
-  }
+  };
 
   private SetupControllers(): void {
     const forecastController = new ForecastController();
 
     this.addControllers([forecastController]);
+  };
+
+  private async databaseSetup(): Promise<void> {
+    await database.connect();
+  };
+
+  public async close() {
+    await database.close();
   }
+
+  public GetApp(): Application {
+    return this.app;
+  };
 }
