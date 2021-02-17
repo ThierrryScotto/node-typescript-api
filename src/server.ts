b69ff66b65244f529ac40f0f6674741a7dfb709a
thierrryScotto <thierry_192@outlook.com>
2021-02-17 02:36:50 -0300

    added users

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/forecast';
 import * as database from '@src/database';
 import { BeachesController } from './controllers/beaches';
+import { UsersController } from './controllers/users';
 
 export class SetupServer extends Server {
   /*
@@ -34,7 +35,13 @@ export class SetupServer extends Server {
   private setupControllers(): void {
     const forecastController = new ForecastController();
     const beachesController = new BeachesController();
-    this.addControllers([forecastController, beachesController]);
+    const usersController = new UsersController();
+
+    this.addControllers([
+      forecastController, 
+      beachesController,
+      usersController
+    ]);
   }
 
   public GetApp(): Application {
